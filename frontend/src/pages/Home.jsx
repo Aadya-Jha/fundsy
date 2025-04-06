51bb717477b1ac4356b7b813ccabda65fadc46ae
sanskriti2004 <sanskritigogoi2004@gmail.com>
2025-04-06 14:48:00 +0530

    navbar

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <main className="bg-beige-light">Welcome</main>
+    </div>
+  );
+};
+
+export default Home;
